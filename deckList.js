ac7bf9c6dbd86a5a0bcf29d6640ce68371f399c2
Ethan Formichella <monkeyscantcry@gmail.com>
2016-10-27 16:21:03 -0400

    Working on making deck lists show up

--- deckList.js
+++ deckList.js
@@ -1,7 +1,38 @@
 function makeDeckList(data) {
-  console.log('Make deck list with', data)
-/*    document.getElementById('deckinfo').innerHTML = deckData.map(function (id) {
-      return '<img src="https://art.hearthstonejson.com/v1/tiles/' + id + '.png" class="cardBanner" /><br/>'
-    }).join('');
-  }*/
+  var cardList = d3.nest()
+    .key(function (d) { return d })
+    .rollup(function (v) { return v.length })
+    .entries(eval(data.card_list))
+
+  var idList = d3.nest()
+    .key(function (d) { return d })
+    .rollup(function (v) { return v.length })
+    .entries(eval(data.card_ids))
+
+  var formattedData = cardList.map(function (d, i) {
+    return {
+      name: d.key,
+      count: d.value,
+      id: idList[i].key
+    }
+  })
+
+  var cards = d3.select('.deckinfo')
+    .select('.deckContents')
+    .selectAll('.card')
+    .data(formattedData)
+
+  cards.enter()
+    .append('div')
+    .classed('card', true)
+    .attr('width', '100%')
+    .html(function (d) { return d.name + ' - ' + d.count })
+    .style('background-image', function (d) { return 'url(https://art.hearthstonejson.com/v1/tiles/' + d.id + '.png)' })
+
+  cards.exit()
+    .remove()
+
+  cards
+    .html(function (d) { return d.name + ' - ' + d.count })
+    .style('background-image', function (d) { return 'url(https://art.hearthstonejson.com/v1/tiles/' + d.id + '.png)' })
 }
