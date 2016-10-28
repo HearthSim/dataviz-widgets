a6e2b77778c6f4debfd9c3123e3cafd970b511ab
Max Walker <maxwell.a.walker@gmail.com>
2016-10-28 17:02:47 -0400

    Better sidebar shorter graphic

--- deckList.js
+++ deckList.js
@@ -1,4 +1,4 @@
-function makeDeckList(data) {
+function getDecklist(data) {
   var cardList = d3.nest()
     .key(function (d) { return d })
     .rollup(function (v) { return v.length })
@@ -17,6 +17,37 @@ function makeDeckList(data) {
     }
   })
 
+  return {
+    archetype: data.archetype,
+    deckId: data.deck_id,
+    matchCount: data.match_count,
+    winRate: data.win_rate,
+    cardList: cardList,
+    idList: idList,
+    formattedData: formattedData
+  }
+}
+
+
+function makeDeckList(deck) {
+  var cardList = deck.cardList;
+  var idList = deck.idList;
+  var formattedData = deck.formattedData;
+
+  var archetype = deck.archetype.replace('_', ' ').toLowerCase()
+
+  d3.select('.deck-id')
+    .text(deck.deckId);
+
+  d3.select('.deck-archetype')
+    .text(archetype);
+
+  d3.select('.deck-matches')
+    .text(deck.matchCount);
+
+  d3.select('.deck-winrate')
+    .text(deck.winRate + '%');
+
   var cards = d3.select('.deckinfo')
     .select('.deckContents')
     .selectAll('.card')
@@ -25,14 +56,15 @@ function makeDeckList(data) {
   cards.enter()
     .append('div')
     .classed('card', true)
-    .attr('width', '100%')
-    .html(function (d) { return d.name + ' - ' + d.count })
+    .style('width', '100%')
+    // .style('height', '32px')
+    .html(function (d) { return '<span class="count">' + d.count + '</span>' + d.name  })
     .style('background-image', function (d) { return 'url(https://art.hearthstonejson.com/v1/tiles/' + d.id + '.png)' })
 
   cards.exit()
     .remove()
 
   cards
-    .html(function (d) { return d.name + ' - ' + d.count })
+    .html(function (d) { return '<span class="count">' + d.count + '</span>' + d.name  })
     .style('background-image', function (d) { return 'url(https://art.hearthstonejson.com/v1/tiles/' + d.id + '.png)' })
 }
